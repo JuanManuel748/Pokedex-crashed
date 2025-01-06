1b79f1d953f3264ad4e0c34519ae00678d9888b7
JuanMa748 <junperz163@gmail.com>
2025-01-06 13:10:30 +0100

    parteJuanma Grafica terminada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FotoPokemonComponent } from "../../components/foto-pokemon/foto-pokemon.component";
 import { TarjetaPokemonComponent } from "../../components/tarjeta-pokemon/tarjeta-pokemon.component";
+import { PokemonService } from '../../services/pokemon.service';
+import { Result } from '../../interfaces/pokeapi';
+import { Pokemon } from '../../interfaces/pokemon';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FotoPokemonComponent, TarjetaPokemonComponent],
+  imports: [CommonModule, FotoPokemonComponent, TarjetaPokemonComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  pokemonList:Result[] = [];
+  page:number = 1;
+  loading:boolean = false;
+  pokemonSelected?:Pokemon;
 
+
+  @ViewChild('tarjetas') tarjetasElement:ElementRef | undefined
+
+  constructor(private pokemonSv: PokemonService) {}
+
+
+
+  ngOnInit(): void {
+    this.loadList();
+  }
+
+  async loadList() {
+    if (this.loading) return;
+    this.loading = true;
+    this.pokemonList = [...this.pokemonList, ...await this.pokemonSv.getByPage(this.page)];
+    this.page++;
+    this.loading = false;
+  }
+
+  onScroll(e:any) {
+    if (
+      Math.round(
+        this.tarjetasElement?.nativeElement.clientHeight + this.tarjetasElement?.nativeElement.scrollTop
+      ) === e.srcElement.scrollHeight
+    ) {
+      this.loadList();
+    }
+  }
+
+  async tarjetaClick(e:string) {
+    console.log(e);
+    this.pokemonSelected = await this.pokemonSv.getById(e);
+  }
 }
