bbac424dba3e11acdae648e7cd7146a53743ea5f
Juan Manuel Pérez <junperz163@gmail.com>
2025-01-10 19:48:09 +0100

    arreglar rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router'; // Importa RouterModule
+import { RouterModule } from '@angular/router'; 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
-import { ErrorPageComponent } from './error-page/error-page.component'; // Importa el componente de error
-import { routes } from './app.routes'; // Importa las rutas
+import { ErrorPageComponent } from './pages/error/error-page.component'; 
+import { routes } from './app.routes'; 
 
 @NgModule({
   declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    
     AppComponent,
     HomeComponent,
     LoginComponent,
     PokemonListComponent,
-    ErrorPageComponent // Declara el componente de error
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes) // Configura las rutas
+    ErrorPageComponent 
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
 import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
-import { ErrorPageComponent } from './error-page/error-page.component'; // Importa el componente de error
+import { ErrorPageComponent } from './pages/error/error-page.component'; 
 
 
 export const routes: Routes = [
