3179d66a93ba7c419689aaadfec42bff4290b206
Hanselmito <151913571+Hanselmito@users.noreply.github.com>
2025-01-10 11:28:52 +0100

    pasos del firebase error en el home

---
+++ src/app/models/pokemon.ts
@@ -0,0 +1,16 @@
+import { Sprites } from "../interfaces/pokemon";
+import { Type, PastType } from "../interfaces/pokemon";
+
+export interface pokemon {
+    id?: string;
+    name: string;
+    base_experience: number;
+    height: number;
+    is_default: boolean;
+    order: number;
+    weight: number;
+    location_area_encounters: string;
+    sprites: Sprites;
+    types: Type[];
+    past_types: PastType[];
+}
